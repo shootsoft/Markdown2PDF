6ae1ce38960dc0294ad8c0ba6c5e624eae284c84
yinjun <yinjun@shootsoft.net>
2015-04-02 23:39:43 +0800

    add mac project

---
+++ src/Markdown2PDF.Mac/AppDelegate.designer.cs
@@ -0,0 +1,10 @@
+﻿
+namespace Markdown2PDF.Mac
+{
+	// Should subclass MonoMac.AppKit.NSResponder
+	[MonoMac.Foundation.Register ("AppDelegate")]
+	public partial class AppDelegate
+	{
+	}
+}
+
---
+++ src/Markdown2PDF.Mac/MainWindow.cs
@@ -0,0 +1,36 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using MonoMac.Foundation;
+using MonoMac.AppKit;
+
+namespace Markdown2PDF.Mac
+{
+	public partial class MainWindow : MonoMac.AppKit.NSWindow
+	{
+		#region Constructors
+
+		// Called when created from unmanaged code
+		public MainWindow (IntPtr handle) : base (handle)
+		{
+			Initialize ();
+		}
+		
+		// Called when created directly from a XIB file
+		[Export ("initWithCoder:")]
+		public MainWindow (NSCoder coder) : base (coder)
+		{
+			Initialize ();
+		}
+		
+		// Shared initialization code
+		void Initialize ()
+		{
+			this.Title = "Markdown2PDF For Mac";
+		}
+
+		#endregion
+	}
+}
+
